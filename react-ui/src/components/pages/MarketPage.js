2db8b60f803cae5131791f2ca1390d9d1895d9b1
Erick Mwamodo <erick@merglabs.com>
2018-01-28 12:43:17 +0300

    Market and Startup Page edit

--- react-ui/src/components/pages/MarketPage.js
+++ react-ui/src/components/pages/MarketPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import aboutHeaderImage from "../../assets/img/daniel-olahs.jpg";
 
-const MarketPage = () => (
-    <div>
-        <h1>MarketPage</h1>
+const StartupPage = () => (
+  <div className="wrapper">
+    <div
+      className="documentation page-header page-header-small"
+      style={{ backgroundImage: `url(${aboutHeaderImage})` }}
+    >
+      <div className="filter" />
     </div>
+  </div>
 );
 
-export default MarketPage;
+export default StartupPage;
--- react-ui/src/components/pages/StartupPage.js
+++ react-ui/src/components/pages/StartupPage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import aboutHeaderImage from "../../assets/img/daniel-olahs.jpg";
 
 const StartupPage = () => (
-    <div>
-        <h1>StartupPage</h1>
+  <div className="wrapper">
+    <div
+      className="documentation page-header page-header-small"
+      style={{ backgroundImage: `url(${aboutHeaderImage})` }}
+    >
+      <div className="filter" />
     </div>
+  </div>
 );
 
 export default StartupPage;
