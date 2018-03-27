33e69dd7bc9dd95d74752ca2b9a2bf2bfc4d1a9c
Zeus <erick@merglabs.com>
2018-03-27 14:16:48 +0300

    Heroku let

--- routes/views/about.js
+++ routes/views/about.js
@@ -1,7 +1,7 @@
 const keystone = require("keystone");
 
-exports = module.exports = function (req, res) {
-  let view = new keystone.View(req, res),
+exports = module.exports = function(req, res) {
+  var view = new keystone.View(req, res),
     locals = res.locals;
 
   locals.section = "about";
